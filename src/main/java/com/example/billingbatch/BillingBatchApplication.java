f0fbf5899c9a2e180e8cb0837ea1ba4580e6eb11
JalHaeHyo <sangok237@gmail.com>
2026-01-16 16:53:33 +0900

    feat: 스케줄러 추가

--- src/main/java/com/example/billingbatch/BillingBatchApplication.java
+++ src/main/java/com/example/billingbatch/BillingBatchApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling // TODO 스케줄러&테이블명 수정 이후 주석 지우기
+@EnableScheduling
 public class BillingBatchApplication {
 
   public static void main(String[] args) {
